6421852321a69c8790ffbd054b95389533a1370c
IgorGarciaCosta <igonildo7@gmail.com>
2017-07-11 12:29:33 -0300

    Update Mercadoria.java

--- Mercadoria.java
+++ Mercadoria.java
@@ -3,20 +3,27 @@ package model;
 
 public class Mercadoria {
 
+    private String lote;	
+    private String Endereco;//o numero chave
     private String bloco;
-    private String lote;
+    private String numero;
     private String fornecedor;
     private String data;
     private String hora;
     
-    public Mercadoria(String b, String l, String f, String d, String h){
-    	this.bloco = b;
+    public Mercadoria(String l, String e, String b, String n, String f, String d, String h){
+	    
     	this.lote = l;
+	this.endereco = e;
+    	this.bloco = b;
+	this.numero = n;
     	this.fornecedor = f;
     	this.data = d;
     	this.hora = h;
     }
-    
+    public String (){
+    	return this.lote + ";" + this.endereco + ";" + this.bloco + ";" + this.numero + ";" + this.fornecedor + ";" + this.data + ";" + this.hora;
+    }
 	public String getBloco() {
 		return bloco;
 	}
