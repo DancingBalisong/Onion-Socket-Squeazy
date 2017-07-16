fcd51c42fae65235ff3eda9c5ef3ae71f6adaf87
IgorGarciaCosta <igonildo7@gmail.com>
2017-07-16 15:10:53 -0300

    Update Controller.java

--- controller/Controller.java
+++ controller/Controller.java
@@ -1,6 +1,6 @@
 /***************************
-Autor: Rafael das quebrada e Igor Garcia
-Componente Curricular: 
+Autor: Rafael Araújo e Igor Garcia
+Componente Curricular: MI programação 
 Concluído em: 16/07/2017
 Declaro que este código foi elaborado por nós de forma individual e não contém nenhum
 trecho de código de outro colega ou de outro autor, tais como provindos de livros e
@@ -8,7 +8,6 @@ apostilas, e páginas ou documentos eletrônicos da Internet. Qualquer trecho de
 de outra autoria que não a minha está destacado com uma citação para o autor e a fonte
 do código, e estamos cientes que estes trechos não serão considerados para fins de avaliação.
 ******************************/
-
 
 package controller;
 
