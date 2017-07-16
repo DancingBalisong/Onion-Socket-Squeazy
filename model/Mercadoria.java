7e5fe549502aab5517989ee6772ad1f54ea84a0d
IgorGarciaCosta <igonildo7@gmail.com>
2017-07-16 14:56:40 -0300

    Update Mercadoria.java

--- model/Mercadoria.java
+++ model/Mercadoria.java
@@ -1,4 +1,13 @@
-
+/***************************
+Autor: Rafael das quebrada e Igor Garcia
+Componente Curricular: 
+Concluído em: 16/07/2017
+Declaro que este código foi elaborado por nós de forma individual e não contém nenhum
+trecho de código de outro colega ou de outro autor, tais como provindos de livros e
+apostilas, e páginas ou documentos eletrônicos da Internet. Qualquer trecho de código
+de outra autoria que não a minha está destacado com uma citação para o autor e a fonte
+do código, e estamos cientes que estes trechos não serão considerados para fins de avaliação.
+******************************/
 package model;
 
 public class Mercadoria {
